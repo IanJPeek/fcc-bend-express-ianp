c4df44a50ab4a12f1906001235b5f866f56a413e
IanJPeek <IanJPeek@gmail.com>
2021-11-27 19:32:17 +0000

    added json path & get response

--- myApp.js
+++ myApp.js
@@ -5,13 +5,17 @@ console.log("Hello World")
 
 const absolutePath = __dirname + "/views/index.html";
 const middlewarePath = __dirname + "/public";
+const reqObj = {
+  "message": "Hello json"
+}
 
 // console.log(middlewarePath)
 
 app.use("/public",express.static(middlewarePath))
 
-  app.get("/", function(req, res) {
-    res.sendFile(absolutePath);
+  app.get("/json", function(req, res) {
+    // res.sendFile(absolutePath);
+    res.json(reqObj)
   });
 
 
