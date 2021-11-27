e488312c7944e3c340e517f89a8098f444e9424f
IanJPeek <IanJPeek@gmail.com>
2021-11-27 16:51:51 +0000

    updated get to sendfile

--- myApp.js
+++ myApp.js
@@ -3,7 +3,11 @@ var app = express();
 
 console.log("Hello World")
 
-app.get("/", function(req, res) {res.send("Hello Express")} )
+const absolutePath = __dirname + /views/index.html;
+
+  app.get("/", function(req, res) {
+    res.sendFile(absolutePath);
+  });
 
 
 
