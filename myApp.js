65b8986f3126acc6653bdeeaba076f6cc6fde17e
IanJPeek <IanJPeek@gmail.com>
2021-11-27 19:40:32 +0000

    added uppercase response depending on env variable

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var express = require('express');
 var app = express();
 
@@ -8,6 +9,9 @@ const middlewarePath = __dirname + "/public";
 const reqObj = {
   "message": "Hello json"
 }
+const upperReqObj = {
+  "message": "HELLO JSON"
+}
 
 // console.log(middlewarePath)
 
@@ -15,7 +19,9 @@ app.use("/public",express.static(middlewarePath))
 
   app.get("/json", function(req, res) {
     // res.sendFile(absolutePath);
-    res.json(reqObj)
+    process.env.MESSAGE_STYLE === "uppercase"
+      ? res.json(upperReqObj)
+      : res.json(reqObj)
   });
 
 
